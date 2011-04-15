d108928af3cf5d0d8c27c254b93dbd3f42e9f51c
Ahmad M Yahya <amyahya97@gmail.com>
2011-04-16 00:32:30 +0700

    fix styling metabox on admin area using css to control input=text size, since defining size= on input att might render differently on browsers

--- twentyten+1/mymetabox.php
+++ twentyten+1/mymetabox.php
@@ -3,13 +3,13 @@
 	<h4>Facebook App Details</h4>
 	<dl>
 	<dt><label for="_myfbapp[app_id]"><?php echo  __("Fb App ID", 'myfbpage_textdomain' ); ?> </label> </dt>
-		 <dd><input type="text" id="_myfbapp[app_id]" name="_myfbapp[app_id]" value="<?php if(!empty($myfbapp['app_id'])) echo $myfbapp['app_id']; ?>" size="64" /></dd>
+		 <dd><input type="text" id="_myfbapp[app_id]" name="_myfbapp[app_id]" value="<?php if(!empty($myfbapp['app_id'])) echo $myfbapp['app_id']; ?>"  /></dd>
 				
 	<dt><label for="_myfbapp[api_key]"><?php echo  __("Fb App Api Key", 'myfbpage_textdomain' ); ?> </label> </dt>
-			<dd><input type="text" id="_myfbapp[api_key]" name="_myfbapp[api_key]" value="<?php if(!empty($myfbapp['api_key'])) echo $myfbapp['api_key']; ?>" size="64" /></dd>
+			<dd><input type="text" id="_myfbapp[api_key]" name="_myfbapp[api_key]" value="<?php if(!empty($myfbapp['api_key'])) echo $myfbapp['api_key']; ?>"  /></dd>
 				
 	<dt><label for="_myfbapp[app_secret]"><?php echo  __("Fb App Secret", 'myfbpage_textdomain' ); ?> </label> </dt>
-			<dd><input type="text" id="_myfbapp[app_secret]" name="_myfbapp[app_secret]" value="<?php if(!empty($myfbapp['app_secret'])) echo $myfbapp['app_secret']; ?>" size="64" /></dd>	
+			<dd><input type="text" id="_myfbapp[app_secret]" name="_myfbapp[app_secret]" value="<?php if(!empty($myfbapp['app_secret'])) echo $myfbapp['app_secret']; ?>"  /></dd>	
 	
 	</dl>
 	<hr class="clear" />
