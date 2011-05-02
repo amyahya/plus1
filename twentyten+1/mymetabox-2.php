2ef96204ec8c17aebb01a71bc6b01150336199f2
Ahmad M Yahya <amyahya97@gmail.com>
2011-05-02 15:56:00 +0700

    using zoom-thumbnails and show selected for skins. functions added, need better position & style

--- twentyten+1/mymetabox-2.php
+++ twentyten+1/mymetabox-2.php
@@ -11,25 +11,39 @@
 	<dt><?php echo  __("Select Skin", 'myfbpage_textdomain' ); ?> </dt>
 			<dd class="skin_options">
 			<label class="skin_opt" >
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/skins/fbstyle-00.png" width="200" alt="fbstyle-00" />
-			<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-00" <?php if ( (empty($myfbapp['fbskin'])) || ($myfbapp['fbskin'] == 'fbstyle-00') ) {echo "checked=1";} ?> /> Default css </label>
+				<a href="<?php echo STYLESHEET_URL ?>/skins/fbstyle-00.png">
+				<img src="<?php echo STYLESHEET_URL ?>/skins/fbstyle-00.png" width="200" alt="fbstyle-00" />
+				<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-00" <?php if ( (empty($myfbapp['fbskin'])) || ($myfbapp['fbskin'] == 'fbstyle-00') ) {echo "checked=1";} ?> /> Default css
+				</a> 
+				</label>
 			<label class="skin_opt" >
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/skins/fbstyle-01.png" width="200" alt="fbstyle-01" />
-			<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-01" <?php if ($myfbapp['fbskin'] == 'fbstyle-01') {echo "checked=1";}?> /> CoolProf </label>
+				<a href="<?php echo STYLESHEET_URL ?>/skins/fbstyle-01.png">
+					<img src="<?php echo STYLESHEET_URL ?>/skins/fbstyle-01.png" width="200" alt="fbstyle-01" />
+					<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-01" <?php if ($myfbapp['fbskin'] == 'fbstyle-01') {echo "checked=1";}?> /> CoolProf 
+					</a>
+				</label>
 			<label class="skin_opt" >
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/skins/fbstyle-02.png" width="200" alt="fbstyle-02" />
-			<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-02" <?php if ($myfbapp['fbskin'] == 'fbstyle-02') {echo "checked=1";}?> /> WebPro2 </label>
+				<a href="<?php echo STYLESHEET_URL ?>/skins/fbstyle-02.png">
+				<img src="<?php echo STYLESHEET_URL ?>/skins/fbstyle-02.png" width="200" alt="fbstyle-02" />
+				<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-02" <?php if ($myfbapp['fbskin'] == 'fbstyle-02') {echo "checked=1";}?> /> WebPro2 
+				</a>
+				</label>
 			<label class="skin_opt" >
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/skins/fbstyle-03.png" width="200"  alt="fbstyle-03" />
-			<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-03" <?php if ($myfbapp['fbskin'] == 'fbstyle-03') {echo "checked=1";}?> /> K2 </label>
+				<img src="<?php echo STYLESHEET_URL ?>/skins/fbstyle-03.png" width="200"  alt="fbstyle-03" />
+				<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-03" <?php if ($myfbapp['fbskin'] == 'fbstyle-03') {echo "checked=1";}?> /> K2 
+				</label>
 			<label class="skin_opt" >
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/skins/fbstyle-04.png" width="200"  alt="fbstyle-04" />
-			<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-04" <?php if ($myfbapp['fbskin'] == 'fbstyle-04') {echo "checked=1";}?> /> Bold Retro </label>
+				<img src="<?php echo STYLESHEET_URL ?>/skins/fbstyle-04.png" width="200"  alt="fbstyle-04" />
+				<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-04" <?php if ($myfbapp['fbskin'] == 'fbstyle-04') {echo "checked=1";}?> /> Bold Retro 
+				</label>
 			<label class="skin_opt" >
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/skins/fbstyle-05.png" width="200"  alt="fbstyle-05" />
-			<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-05" <?php if ($myfbapp['fbskin'] == 'fbstyle-05') {echo "checked=1";}?> /> Batik Classic </label>
-			</dd>		
-					
+				<img src="<?php echo STYLESHEET_URL ?>/skins/fbstyle-05.png" width="200"  alt="fbstyle-05" />
+				<input type="radio" name="_myfbapp[fbskin]"  value="fbstyle-05" <?php if ($myfbapp['fbskin'] == 'fbstyle-05') {echo "checked=1";}?> /> Batik Classic 
+				</label>
+			<div id="selectedskin">
+				<img src="<?php echo STYLESHEET_URL ?>/skins/fbstyle-00.png" width="200" alt="fbstyle-00" />
+				</div>
+			</dd>							
 	</dl>	
 	
 	
---
+++ twentyten+1/plus1.js
@@ -0,0 +1,46 @@
+﻿/* +1 javascript */
+
+/* zoom and display thumbnails on metabox skin options */
+/* adapted from http://www.sohtanaka.com/web-design/fancy-thumbnail-hover-effect-w-jquery/ */
+
+jQuery(document).ready(function($){
+ 
+//Larger thumbnail preview 
+ 
+$("dd.skin_options label").hover(function() {
+	$(this).css({'z-index' : '10'});
+	$(this).find('img').addClass("hover").stop()
+		.animate({
+			marginTop: '-110px', 
+			marginLeft: '-110px', 
+			top: '50%', 
+			left: '50%', 
+			width: '174px', 
+			height: '174px',
+			padding: '20px' 
+		}, 200);
+	
+	} , function() {
+	$(this).css({'z-index' : '0'});
+	$(this).find('img').removeClass("hover").stop()
+		.animate({
+			marginTop: '0', 
+			marginLeft: '0',
+			top: '0', 
+			left: '0', 
+			width: '100px', 
+			height: '100px', 
+			padding: '5px'
+		}, 400);
+});
+ 
+//Swap Image on Click
+	$("dd.skin_options label a").click(function() {
+		
+		var mainImage = $(this).attr("href"); //Find Image Name
+		$("#selectedskin img").attr({ src: mainImage });
+		return false;		
+	});
+ 
+});
+
